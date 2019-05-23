eba9920a8029c73d0d17e69bc6cc9a939277cbc6
Anders Jensen-Urstad <anje0901@student.miun.se>
2019-05-23 16:47:07 +0200

    Make it possible to add tweets/see tweets on index page

--- tweets/views.py
+++ tweets/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from .models import Post, Vote
-from django import forms
+from .forms import CreatePostForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
-
-
-class CreatePostForm(forms.Form):
-    post_text = forms.CharField(widget=forms.Textarea( \
-        attrs={"rows":10, "cols":60}), label='Tweet away', required=True)
-
-# this class is not used at the moment (consider refactor?)
-class LoginForm(forms.Form):
-    username = forms.CharField(label="User name", required="True")
-    password = forms.CharField(label="Password", required=True, widget=forms.PasswordInput())
-
 
 def index(request):
     """View function for home page of site."""
@@ -23,35 +13,29 @@ def index(request):
     all_posts = Post.objects.all()
 
     context = {
+        'form': CreatePostForm(),
         'posts': all_posts
     }
 
     return render(request, 'index.html', context=context)
 
-
 @login_required
+@require_http_methods(["POST"])
 def create_post(request):
-
-    if request.method == "POST":
-        filled_out_form = CreatePostForm(request.POST)
-        if filled_out_form.is_valid():
-            post_text = filled_out_form.cleaned_data['post_text']
-            post = Post.objects.create(text=post_text, author=request.user)
-            return render(request, 'index.html')
-        else:
-            # handle this
-            pass
+    filled_out_form = CreatePostForm(request.POST)
+    if filled_out_form.is_valid():
+        post_text = filled_out_form.cleaned_data['post_text']
+        post = Post.objects.create(text=post_text, author=request.user)
+        messages.add_message(request, messages.INFO, 'Tweet successfully tweeted!')
     else:
-        create_post_form = CreatePostForm()
+        messages.add_message(request, messages.WARNING, 'Form data invalid.')
 
-    return render(request, 'create_post_form.html', {'form': create_post_form})
-
+    return HttpResponseRedirect("/")
 
 def display_all_posts_by_user(request, user_id):
     all_posts = Post.objects.filter(author=user_id)
 
     return render(request, 'index.html', {'posts': all_posts })
-
 
 @login_required
 @require_http_methods(["POST"])
@@ -85,7 +69,6 @@ def vote_up(request):
 
     return render(request, 'index.html', {'message': 'Tweet upvoted'})
 
-
 @login_required
 @require_http_methods(["POST"])
 def vote_down(request):
@@ -100,13 +83,11 @@ def vote_down(request):
 
     return render(request, 'index.html', {'message': 'Tweet downvoted'})
 
-
 def login(request):
     # todo: log user in
 
     # change this
     return render(request, 'index.html', {'message': 'logged in'})
-
 
 def logout(request):
     # todo: log user out

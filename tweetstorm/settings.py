fd42604e404c4e9b26f6c0ded6144b1d1d9b33ee
Anders Jensen-Urstad <anje0901@student.miun.se>
2019-05-28 18:29:49 +0200

    Change STATIC_ROOT to better accommodate VirtualBox appliance

--- tweetstorm/settings.py
+++ tweetstorm/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/static')
 STATIC_URL = '/static/'
 
 # Where to redirect user after login/logout

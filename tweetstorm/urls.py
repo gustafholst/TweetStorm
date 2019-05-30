95ca0076402e2e1f3a9d64c544b4ff9ef966bf77
Anders Jensen-Urstad <anje0901@student.miun.se>
2019-05-30 15:58:54 +0200

    Add login history to account security page

--- tweetstorm/urls.py
+++ tweetstorm/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 from django.views.generic import RedirectView
 from django.urls import include
 from django.contrib.auth import views as auth_views
-from tweets.views import IndexView, CustomLoginView, CustomRegistrationView
+from tweets.views import IndexView, CustomLoginView, CustomRegistrationView, AccountSecurityView
 from two_factor.urls import urlpatterns as tf_urls
 
 # https://docs.djangoproject.com/en/2.2/topics/auth/default/#using-the-views
@@ -35,13 +35,14 @@ from two_factor.urls import urlpatterns as tf_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweets/', include('tweets.urls')),
-    path('accounts/', include('django_registration.backends.one_step.urls')),
-    path('accounts/', CustomRegistrationView.as_view(), name='django_registration_register'),
+    path('account/', include('django_registration.backends.one_step.urls')),
+    path('account/', CustomRegistrationView.as_view(), name='django_registration_register'),
     #path('accounts/login/', auth_views.LoginView.as_view(), name='login'), # custom login view used for ratelimit mixin
     ###path('accounts/login/', CustomLoginView.as_view(), name='login'),
     #path('accounts/logout/', auth_views.LogoutView.as_view()), # we use our own view for this, as Django's default one lacks CSRF protection
-    path('accounts/change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password_form.html', success_url='/accounts/change-password/done/'), name='change_password'),
-    path('accounts/change-password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change_password_done.html'), name='change_password_done'),
+    path('account/change-password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password_form.html', success_url='/accounts/change-password/done/'), name='change_password'),
+    path('account/change-password/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/change_password_done.html'), name='change_password_done'),
+    path('account/two_factor/', AccountSecurityView.as_view(), name='account_security'),
     path('', include(tf_urls)),
     path('', IndexView.as_view()),
     #path('', RedirectView.as_view(url='/tweets/', permanent=True)),

97babe6d7bbefc697cc0d79089c712a8b26a09f5
Anders Jensen-Urstad <anje0901@student.miun.se>
2019-05-30 18:33:23 +0200

    Login notification wording

--- tweets/signals.py
+++ tweets/signals.py
@@ -10,7 +10,7 @@ def user_logged_in_callback(sender, request, user, **kwargs):
     try:
         previous_login = LoginLog.objects.filter(username__exact=user.username).order_by('-timestamp')[1]
         previous_login_time = timesince(previous_login.timestamp)
-        messages.add_message(request, messages.INFO, f"Welcome back {user.username}! You were last logged in {previous_login_time} ago from {previous_login.ip_address} with {previous_login.user_agent}")
+        messages.add_message(request, messages.INFO, f"Welcome back {user.username}! You previusly logged in {previous_login_time} ago from {previous_login.ip_address} with {previous_login.user_agent}")
     except IndexError:
         # User has logged in for the first time: an "account created" page is shown,
         # so no need to set a message

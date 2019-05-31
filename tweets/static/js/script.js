b6a35354ea401c1ba0daadc06d06307221ccfb37
Gustaf Holst <guho1700@student.miun.se>
2019-05-31 09:58:46 +0200

    linted js script

--- tweets/static/js/script.js
+++ tweets/static/js/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
-  $('form.cansubmit .vote').click(function() {
+  $("form.cansubmit .vote").click(function() {
 
-    const post_id = $(this).parents('form').children('input[name=post_id]').val();
-    const csrf = $(this).parents('form').children('input[name=csrfmiddlewaretoken]').val();
-    const vote = $(this).hasClass('fa-thumbs-up') ? 1 : -1
+    const post_id = $(this).parents("form")
+                          .children("input[name=post_id]").val();
+    const csrf = $(this).parents("form")
+                          .children("input[name=csrfmiddlewaretoken]").val();
+    const vote = ($(this).hasClass("fa-thumbs-up") ? 1 : -1);
 
     // reset vote
-    $(this).parent().find('i').removeClass('text-success text-danger');
+    $(this).parent().find("i").removeClass("text-success text-danger");
 
-    data = {
+    const data = {
       "post_id": post_id,
       "vote": vote,
-      'csrfmiddlewaretoken': csrf
-    }
+      "csrfmiddlewaretoken": csrf
+    };
 
     $.ajax({
          type: "POST",
@@ -21,22 +23,24 @@ $(document).ready(function(){
          dataType: "json",
          success: function(json) {
 
-            const post_id = json['post_id'];
-            const vote = json['vote'];
+            const ret_post_id = json.post_id;
+            const new_vote = json.vote;
 
-            const input_element = $("input[value=" + post_id + "]");
+            const input_element = $("input[value=" + ret_post_id + "]");
 
             // set new vote counts
-            input_element.siblings('.up_count').text(json['num_up_votes']);
-            input_element.siblings('.down_count').text(json['num_down_votes']);
+            input_element.siblings(".up_count").text(json.num_up_votes);
+            input_element.siblings(".down_count").text(json.num_down_votes);
 
             //mark new vote
-            if (vote == 1)
-              input_element.siblings('.fa-thumbs-up').addClass('text-success');
+            if (new_vote === 1) {
+              input_element.siblings(".fa-thumbs-up").addClass("text-success");
+            }
 
-            if (vote == -1)
-              input_element.siblings('.fa-thumbs-down').addClass('text-danger');
-          },
+            if (new_vote === -1) {
+              input_element.siblings(".fa-thumbs-down").addClass("text-danger");
+            }
+          }
     });
   });
 });

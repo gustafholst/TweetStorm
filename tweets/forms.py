bce8af33a7670133dffdd2af1a9c4c739d4ef89a
Anders Jensen-Urstad <anje0901@student.miun.se>
2019-06-02 02:44:44 +0200

    Add max length to tweet form

--- tweets/forms.py
+++ tweets/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class CreatePostForm(forms.Form):
     """Form for tweeting."""
     post_text = forms.CharField(widget=forms.Textarea(
-        attrs={"rows": 3, "cols": 30}), label='Tweet away', required=True)
+        attrs={"rows": 3, "cols": 30, "maxlength": 280}), label='Tweet away (280 characters max)', required=True)
